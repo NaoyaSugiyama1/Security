101050f49e9190867f64a17fbf9a468ac5504650
norm-ideal <idehara.n@gmail.com>
2020-07-24 15:16:38 +0900

    Car Control

---
+++ Assets/test-idehara/Car/CarController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // 運転手いらいら度
+    public float frustration;
+    public float speed;
+    public float accel;
+
+    public GameObject target;   // 目的地（空ゲームオブジェクト推奨）
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frustration = 0;
+        accel = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        transform.position += dir.normalized * speed * Time.deltaTime;
+    }
+
+    // 運転手が受け入れたら true
+    public bool Order(GestureManager.GestureType gesture)
+    {
+        switch( gesture )
+        {
+            case GestureManager.GestureType.STOP:
+                speed = 0;
+                break;
+            case GestureManager.GestureType.SLOW:
+                speed = 1.0f;
+                break;
+            case GestureManager.GestureType.GO:
+                speed = 3.0f;
+                break;
+        }
+        return true;
+    }
+
+    public void SetTarget(GameObject t)
+    {
+        target = t;
+    }
+}
---
+++ Assets/test-idehara/GameLoop/GestureManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureManager : MonoBehaviour
+{
+    public enum GestureType { STOP, SLOW, GO };
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
