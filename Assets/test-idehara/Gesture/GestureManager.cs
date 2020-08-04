76a8e965c46f1b899fa7b4857ea5400395594a1c
NaoyaSugiyama1 <49972784+NaoyaSugiyama1@users.noreply.github.com>
2020-08-04 17:32:09 +0900

    Kinectの接続に成功

--- Assets/test-idehara/Gesture/GestureManager.cs
+++ Assets/test-idehara/Gesture/GestureManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Windows.Kinect;
+using System.Linq;
 
 public class GestureManager : MonoBehaviour
 {
     public enum GestureType { STOP, SLOW, GO, NULL };
+    public BodySourceManager _BodyManager;
 
     // 認識結果をインスペクタ上で↓この変数をチェックしながらコーディング
     public GestureType currentGesture;
@@ -21,6 +24,35 @@ public class GestureManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_BodyManager == null)
+        {
+            Debug.Log("_BodyManager == null");
+            return;
+        }
+
+        // Bodyデータを取得する
+        var data = _BodyManager.GetData();
+        if (data == null)
+        {
+            return;
+        }
+
+        // 最初に追跡している人を取得する
+        var body = data.FirstOrDefault(b => b.IsTracked);
+        if (body == null)
+        {
+            return;
+        }
+
+        var Rhand = body.Joints[JointType.HandRight];
+        var Relbow = body.Joints[JointType.ElbowRight];
+        var Rshoulder = body.Joints[JointType.ShoulderRight];
+        var Lhand = body.Joints[JointType.HandLeft];
+        var Lelbow = body.Joints[JointType.ElbowLeft];
+        var Lshoulder = body.Joints[JointType.ShoulderLeft];
+        Debug.Log(Rhand);
+
+
         // StickRecognizer である sr に、棒の位置や動きを問い合わせ、
         // 関節情報などを加味して、ジェスチャを認識する。
         // ジェスチャを認識したら、currentGesture と duration を更新する
@@ -28,7 +60,7 @@ public class GestureManager : MonoBehaviour
         // 新しいジェスチャが来たら、duration を 0 でリセット
 
         // デバッグ用
-        if( Input.GetKey( KeyCode.W) ) {
+        if ( Input.GetKey( KeyCode.W) ) {
             if( currentGesture != GestureType.GO ) duration = 0;
             currentGesture = GestureType.GO;
         }
