fc6f0b24c877e4fa051eadcacac35954d847af03
norm-ideal <idehara.n@gmail.com>
2020-08-06 12:58:47 +0900

    コーナリング安定性の向上（72km/h まで対応）

--- Assets/test-idehara/Car/CarController.cs
+++ Assets/test-idehara/Car/CarController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// 車ゲームオブジェクトのコライダ設定
+// ・車間距離を維持するためのトリガー（１ｍくらいモデルの外側）
+// ・人や他の車との衝突判定に使うノントリガー（モデル表面）
+
 public class CarController : MonoBehaviour
 {
-    public enum CarState {NORMAL, CRASHED, ARRIVED};
+    public enum CarState {NORMAL, CRASHED, ARRIVED, WAITING};
     // 運転手いらいら度
     public float frustration;
     public float speed;
@@ -17,6 +21,8 @@ public class CarController : MonoBehaviour
     public CarState state;
     // 目的地（空ゲームオブジェクト推奨）
     public GameObject target;
+    // 前を走行する車
+    public GameObject previousCar;
 
     private float wheelAngle;
     private float addAngleFactor = 12.0f;
@@ -37,18 +43,38 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float rotationAngle = Mathf.Tan(wheelAngle*3.14f/180.0f) * addAngleFactor * carLength * speed * Time.deltaTime;
+        Vector3 dir = target.transform.position - transform.position;
+        Vector3 orgforward = transform.forward;
+
+        // 現在の向きで半分進んで
         transform.position += transform.forward * speed * Time.deltaTime /2;
-        transform.Rotate( 0, Mathf.Tan(wheelAngle*3.14f/180.0f) * addAngleFactor * carLength * speed * Time.deltaTime, 0, Space.Self);
+        // 曲がる
+        transform.Rotate( 0, rotationAngle, 0, Space.Self);
+        // 曲がりすぎた結果、目的地が正面を通り過ぎたら
+        if( Vector3.Cross(orgforward, dir).y * Vector3.Cross(transform.forward, dir).y < 0 )
+        {
+            // ハンドルを切り戻していたことにして向きを少し戻す
+            wheelAngle *= 0.5f;
+            transform.Rotate( 0, -rotationAngle*0.5f, 0, Space.Self);
+            Debug.Log("Wheel Back");
+        }
+        // 回転後の向きで半分進む
         transform.position += transform.forward * speed * Time.deltaTime /2;
 
-        if( wheelAngle == 0 && Mathf.Abs(targetPlane.GetDistanceToPoint( transform.position)) < carLength * 1.3f)
+        // 曲がりはじめる条件。車体先端から 30% 先に目的地平面がやってきた
+        if( wheelAngle == 0 && Mathf.Abs(targetPlane.GetDistanceToPoint(transform.position)) < carLength * 1.3f)
             wheelAngle = -45.0f;
 
-        Vector3 dir = target.transform.position - transform.position;
+        // 微調整
+        Vector3 cr = Vector3.Cross(transform.forward.normalized, dir.normalized);
         float leftAngle = Vector3.Angle(dir, transform.forward);
-        Debug.Log(leftAngle);
+        // 目的地が３度以下に見えているならハンドルをどんどん戻す
         if( Mathf.Abs(leftAngle) < 3 )
             wheelAngle *= 0.55f;
+        // 目的地が４０度以下に見えているなら、ずれている角度に応じてハンドルを切り戻す
+        else if( Mathf.Abs(leftAngle) < 40 )
+            wheelAngle += cr.y * 0.8f;
 
         if( state == CarState.ARRIVED )
         {
@@ -81,4 +107,11 @@ public class CarController : MonoBehaviour
         target = t;
         targetPlane = new Plane( transform.forward,  target.transform.position );
     }
+
+    // 駐車場ゲートなど一定時間停止
+    public void WaitAtGate(float t)
+    {
+
+    }
+
 }
