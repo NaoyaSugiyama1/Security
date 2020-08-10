e15b1f56250748f93951c7279b7282aed2907de7
norm-ideal <idehara.n@gmail.com>
2020-08-10 15:51:42 +0900

    WalkerSpawner プレファブを実装

--- Assets/test-idehara/Walker/WalkerManager.cs
+++ Assets/test-idehara/Walker/WalkerManager.cs
@@ -8,10 +8,14 @@ public class WalkerManager : MonoBehaviour
     public float speed;
     public GameObject target;
 
+    private bool isArrived;
+    private Vector3 move;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        isArrived = false;
+        move = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -19,10 +23,11 @@ public class WalkerManager : MonoBehaviour
     {
         if(target)
         {
-            Vector3 move = target.transform.position - gameObject.transform.position;
+            if( !isArrived )
+                move = target.transform.position - gameObject.transform.position;
             if( move.magnitude < ARRIVAL_DIST )
             {
-                speed = 0;
+                isArrived = true;
                 Destroy(gameObject, 0.5f);
             }
             gameObject.transform.position += move.normalized * Time.deltaTime * speed;
